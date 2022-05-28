44c0d8f830f07802a5d44760bc7498a64e556344
Warish Alam <warishalam349@gmail.com>
2022-05-28 16:55:26 +0530

    Second

--- script.js
+++ script.js
@@ -34,23 +34,77 @@ async function fetchData(url){
 function displayData(data){
 
     let card=document.createElement("div");
+    card.id="card";
 
     let title=document.createElement("p");
     title.textContent=data.title;
+   
+    if(data.status){
+        title.color="red";
+        
+    }
     
     let status=document.createElement("input");
     status.type = "checkbox";
     status.checked =data.status;
 
     let editbtn=document.createElement("button");
-    // editbtn.textContent="Edit"
-    // editbtn.setAttribute("onclick"," function(){
-    // //     localStorage.setItem("todoitem",JSON.stringify(data));
-    // //     location.href="https://google.com";
-    // //     // console.log(data);
-    // }");
+    editbtn.textContent="Edit";
+    editbtn.onclick= async function(){
+        try {
+            let data= await fetch(`http://localhost:3000/task/${data.id}`);
+            let tempdata=data.json();
+         //    console.log(tempdata);
+         localStorage.setItem("tempData",JSON.stringify(tempdata));
 
-    card.append(title,status);
+        
+            
+        } catch (error) {
+            console.log(error, "UPDATE Error");
+        }
+
+        alert("Update Sucessfull")
+     }
+     //udtae
+
+    let deletebtn=document.createElement("button");
+    deletebtn.textContent="Delete";
+    deletebtn.onclick=async function(){
+        try {
+         let res=await fetch(`http://localhost:3000/task/${data.id}`,{
+             method:"delete"
+         });
+         // updateDom();
+            
+        } catch (error) {
+            console.log(error, "Delete Error");
+        }
+
+        alert("Delete Sucessfull")
+     }
+
+    card.append(title,status,editbtn,deletebtn);
     document.querySelector("#container").append(card);
 
+}
+
+// function add item
+
+function addItem (){
+
+    console.log("Hello");
+    let title=document.createElement("input");
+    title.id="title-item";
+
+    let status=document.createElement("input");
+    status.type="checkbox";
+    status.id="status-item";
+
+    let add=document.createElement("button");
+    add.textContent="Add Item";
+    add.id="add";
+    add.onclick=function(){
+    
+}
+    document.querySelector("#item-add").append(title,status,add);
 }
